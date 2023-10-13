42ddd307ffc2334586d462059a0e1ef76d8f34ff
Bruno Laudelino <bruno.laudelino@gmail.com>
2023-10-13 01:42:08 -0300

    Update primeiro_programa.py

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,1 +1,24 @@
 print("Olá Mundo !")
+print("Oi, Seja bem vindo ao mundo de Python")
+
+print(11 + 10 + 1000)
+print(1.5 + 1 + 0.5) 
+print(True)
+print("Python")
+
+int()
+float()
+str()
+bool()
+
+age, name = (23, 'Guilherme')
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+print(int(1.9))
+
+nome =input('Informe o seu nome: ')
+idade = input('Informe sua idade: ')
+print("Teste")
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#\n")
+
+print( 5 // 2)
