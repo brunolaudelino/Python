8ba63814e784c342a9e80ad6a63675a76ccefbb7
Bruno Laudelino <bruno.laudelino@gmail.com>
2023-10-14 12:31:20 -0300

    atualização

---
+++ Conceitos Básicos de Python/tipos_de_operadores.py
@@ -0,0 +1,38 @@
+#Operdores Aritméticos
+#Adição
+print(1 +1)
+#Subtração
+print(10 - 2)
+#Multiplicação
+print(4 * 3)
+#Divisão
+print(12 / 3)
+#Divisão inteira
+print(12 // 2)
+#Módulo
+print(10 % 3)
+#Exponenciação
+print(2 ** 3)
+
+#Operadores de Comparação
+saldo = 450
+saque = 200
+
+#igualdade
+print(saldo == saque)
+
+#Diferença
+print(saldo != saque)
+
+#Maior que / Maior ou igual
+
+print(saldo > saque)
+print(saldo >= saque)
+
+Menor que / Menor ou igual
+
+print(saldo < saque)
+print(saldo <= saque)
+#Atibuição
+saldo = 500
+print(saldo)
